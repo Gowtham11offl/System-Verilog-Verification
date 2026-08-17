d8a48155784a5a2323464b91ae1c11d4e5107f1a
Gowtham Kumar S <journeytowardssilicon@gmail.com>
2026-08-17 10:53:45 +0530

    Create interface.sv

---
+++ 2. D Flip Flop/2. Verification/interface.sv
@@ -0,0 +1,20 @@
+interface dff_itf(input clk);
+  
+  logic rst;
+  logic d;
+  logic q;
+  
+  clocking drv_clk @(posedge  clk);
+    default output #0;
+    output rst,d; 
+  endclocking
+  
+  clocking mon_clk @(posedge clk);
+    default input #0;
+    input q,rst,d;
+  endclocking
+
+  modport drive(input clk,clocking drv_clk);
+  modport mon(input clk,clocking mon_clk);
+  
+endinterface
